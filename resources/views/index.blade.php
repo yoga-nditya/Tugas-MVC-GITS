e66dbc2b6f2026c6fa96d164751f7b00deff6871
unknown <yoga_nditya>
2023-03-09 13:57:54 +0700

    inisial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MSIB</title>
+</head>
+<body>
+    <p>Nama : {{$user}}</p>
+
+    <table style="width:100%">
+    <?php $i = 1;?>
+    <tr>
+        <td>No</td>
+        <td>Nama</td>
+        <td>Email</td>
+    </tr>
+    @foreach ($modelUser as $user)
+    <tr>
+        <td>{{ $i }}</td>
+        <td>{{ $user['name'] }}</td>
+        <td>{{ $user['email'] }}</td>
+    </tr>
+    <?php $i++;?>
+    @endforeach
+</table>
+</body>
+</html>
